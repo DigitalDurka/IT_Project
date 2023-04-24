b8d18eff27a15ad1b2aee0e8f09b4717118ac914
DigitalDurka <polev.ea@students.dvfu.ru>
2023-04-24 21:16:40 +1000

    first

---
+++ Domain/Models/Specialization.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models
+{
+    public class Specialization
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Specialization(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
